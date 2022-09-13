27b8b359508e8b219052120fba0df3ee71699b94
Shelby Bridwell <109699848+shelbybridwell@users.noreply.github.com>
2022-09-12 21:23:30 -0500

    still need to update day 5 of forecast, set links to function, uv index, styling

--- assets/script.js
+++ assets/script.js
@@ -123,11 +123,8 @@ function getMeteoByCoordinates(lat, lon, callback){
 
 
 function displayMeteo(data){
-    // Update Google Map URL
-    googleMapCity = "https://www.google.com/maps/place/" + data.city.coord.lat + "," + data.city.coord.lon;
-    $('#meteo-title span').html('Weather in <a href="' + googleMapCity + '" class="text-muted meteo-city" target="_blank">' + data.city.name + ', ' + data.city.country + '</a>');
-    // Update meteo for each day
-    var tempMoyenne = 0;
+   
+    var tempAvg = 0;
     for (var i = 0; i < 6; i++){
         // Get meteo
         meteo = data.list[i*8];
@@ -147,16 +144,9 @@ function displayMeteo(data){
         humidity.text(meteo.main.humidity + "%");
         wind.text(meteo.wind.speed + " mph");
        // uv.text(meto.main.uv + " ");
-        tempMoyenne += meteo.main.temp;
+        tempAvg += meteo.main.temp;
     }
-    displaySunriseSunset(data.city.coord.lat, data.city.coord.lon);
-    // Get custom gradient according to the temperature
-    tempMoyenne = toFarenheit(tempMoyenne / 3);
-    var hue1 = 30 + 240 * (30 - tempMoyenne) / 60;
-    var hue2 = hue1 + 30;
-    rgb1 = 'rgb(' + hslToRgb(hue1 / 360, 0.6, 0.5).join(',') + ')';
-    rgb2 = 'rgb(' + hslToRgb(hue2 / 360, 0.6, 0.5).join(',') + ')';
-    $('body').css('background', 'linear-gradient(' + rgb1 + ',' + rgb2 + ')');
+   ;
 }
 function pad(n, p, c) {
     var pad_char = typeof c !== 'undefined' ? c : '0';
@@ -172,40 +162,4 @@ function toFarenheit(kelvin) {
     return (Math.round(deg)) * 1.8 + 32;
 }
 
-/**
- * Source : http://web.archive.org/web/20081227003853/http://mjijackson.com/2008/02/rgb-to-hsl-and-rgb-to-hsv-color-model-conversion-algorithms-in-javascript
- * Converts an HSL color value to RGB. Conversion formula
- * adapted from http://en.wikipedia.org/wiki/HSL_color_space.
- * Assumes h, s, and l are contained in the set [0, 1] and
- * returns r, g, and b in the set [0, 255].
- *
- * @param   {number}  h       The hue
- * @param   {number}  s       The saturation
- * @param   {number}  l       The lightness
- * @return  {Array}           The RGB representation
- */
- function hslToRgb(h, s, l) {
-    var r, g, b;
-
-    if (s == 0) {
-        r = g = b = l; // achromatic
-    } else {
-        var hue2rgb = function hue2rgb(p, q, t) {
-            if (t < 0) t += 1;
-            if (t > 1) t -= 1;
-            if (t < 1 / 11) return p + (q - p) * 11 * t;
-            if (t < 1 / 2) return q;
-            if (t < 2 / 5.5) return p + (q - p) * (2 / 3 - t) * 11;
-            return p;
-        }
-
-        var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
-        var p = 2 * l - q;
-        r = hue2rgb(p, q, h + 1 / 3);
-        g = hue2rgb(p, q, h);
-        b = hue2rgb(p, q, h - 1 / 3);
-    }
-
-    return [Math.round(r * 255), Math.round(g * 255), Math.round(b * 255)];
-}
 
